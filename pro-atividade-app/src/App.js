1e8f4650652039bd393c5e32abb6e5898677d096
DennyAzevedo <denny_af@yahoo.com.br>
2022-02-16 19:42:57 -0300

    Criando componentes iniciais

--- pro-atividade-app/src/App.js
+++ pro-atividade-app/src/App.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import './App.css';
+import AtividadeForm from './components/AtividadeForm';
+import Atividade from './components/Atividade';
 
 let initialState = [
   {
@@ -30,112 +32,24 @@ function App() {
     setAtividades([...atividades, { ...atividade }]);
   }
 
-  function prioridadeLabel(param) {
-    switch(param) {
-      case '1':
-        return 'Baixa';
-      case '2': 
-        return 'Normal';
-      case '3':
-        return 'Alta';
-      default:
-        return 'Não definido';
-    }
-  }
-
-  function prioridadeStyle(param) {
-    switch(param) {
-      case '1':
-        return 'smile';
-      case '2': 
-        return 'meh';
-      case '3':
-        return 'frown';
-      default:
-        return 'Não definido';
-    }
+  function deletarAtividade(id) {
+    const atividadesFiltradas = atividades.filter(atividade => atividade.id !== id);
+    setAtividades([...atividadesFiltradas]);
   }
 
   return (
     <>
-      <form className="row g-3">
-        <div className="col-md-6">
-          <label className="form-label">ID</label>
-          <input 
-            id="id" 
-            type="text" 
-            className="form-control" 
-          />
-        </div>
-        <div className="col-md-6">
-          <label className="form-label">Prioridade</label>
-          <select id="prioridade" className="form-select">
-            <option defaultValue="0">Selecionar...</option>
-            <option value="1">Baixa</option>
-            <option value="2">Normal</option>
-            <option value="3">Alta</option>
-          </select>
-        </div>
-        <div className="col-md-6">
-          <label className="form-label">Título</label>
-          <input 
-            id="titulo" 
-            type="text" 
-            className="form-control" 
-          />
-        </div>
-        <div className="col-md-6">
-          <label className="form-label">Descrição</label>
-          <input 
-            id="descricao" 
-            type="text" 
-            className="form-control" 
-          />
-        </div>
-        <hr />
-        <div className="col-12">
-          <button 
-            className="btn btn-outline-secondary" 
-            onClick={addAtividade}
-          >
-            + Atividade
-          </button>
-        </div>
-      </form>
+      <AtividadeForm 
+        atividades={atividades}
+        addAtividade={addAtividade}
+      />
       <div className="mt-3">
           {atividades.map(ativ =>(
-            <div key={ativ.id} className="card mb-2 shadow-sm">
-              <div className="card-body">
-                <div className="d-flex justify-content-between">
-                  <h5 className="card-tittle">
-                    <span className="badge bg-secondary me-1">
-                      {ativ.id}
-                    </span>
-                    - {ativ.titulo}
-                  </h5>
-                  <h6>
-                    Prioridade:
-                    <span className="ms-1 text-black">
-                      <i className={'me-1 fa-regular fa-' + prioridadeStyle(ativ.prioridade)}></i>
-                      {prioridadeLabel(ativ.prioridade)}
-                    </span>
-                  </h6>
-                </div>
-                <p className="card-text">
-                  {ativ.descricao}
-                </p>
-                <div className="d-flex justify-content-end pt-2 m-0 border-top">
-                  <button className="btn btn-sm btn-outline-primary me-2">
-                    <i className="fas fa-pen me-2"></i>
-                    Editar
-                  </button>
-                  <button className="btn btn-sm btn-outline-danger">
-                  <i className="fas fa-trash me-2"></i>
-                    Deletar
-                  </button>
-                </div>
-              </div>
-            </div>
+            <Atividade
+              key={ativ.id}
+              ativ={ativ}
+              deletarAtividade={deletarAtividade}
+            />
           ))}
       </div>
     </>
---
+++ pro-atividade-app/src/components/Atividade.js
@@ -0,0 +1,83 @@
+import React from 'react';
+
+export default function Atividade(props) {
+
+  function prioridadeLabel(param) {
+    switch(param) {
+      case '1':
+        return 'Baixa';
+      case '2': 
+        return 'Normal';
+      case '3':
+        return 'Alta';
+      default:
+        return 'Não definido';
+    }
+  }
+
+  function prioridadeStyle(param) {
+    switch(param) {
+      case '1':
+        return 'smile';
+      case '2': 
+        return 'meh';
+      case '3':
+        return 'frown';
+      default:
+        return 'Não definido';
+    }
+  }
+
+  function selectColor(param) {
+    switch(param) {
+      case '1':
+        return 'success';
+      case '2': 
+        return 'secondary';
+      case '3':
+        return 'warning';
+      default:
+        return 'danger';
+    }
+  }
+
+  return (
+    <div className={'card mb-2 shadow-sm border-' + selectColor(props.ativ.prioridade)}>
+      <div className="card-body">
+        <div className="d-flex justify-content-between">
+          <h5 className="card-tittle">
+            <span className="badge bg-secondary me-1">
+              {props.ativ.id}
+            </span>
+            - {props.ativ.titulo}
+          </h5>
+          <h6>
+            Prioridade:
+            <span className={'ms-1 text-' + selectColor(props.ativ.prioridade)}>
+              <i className={'me-1 fa-regular fa-' + prioridadeStyle(props.ativ.prioridade)}></i>
+              {prioridadeLabel(props.ativ.prioridade)}
+            </span>
+          </h6>
+        </div>
+        <p className="card-text">
+          {props.ativ.descricao}
+        </p>
+        <div className="d-flex justify-content-end pt-2 m-0 border-top">
+          <button 
+            className="btn btn-sm btn-outline-primary me-2"
+          >
+            <i className="fas fa-pen me-2"></i>
+            Editar
+          </button>
+          <button 
+            className="btn btn-sm btn-outline-danger" 
+            onClick={() => props.deletarAtividade(props.ativ.id)}
+          >
+            <i className="fas fa-trash me-2"></i>
+            Deletar
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
