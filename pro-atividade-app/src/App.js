42d28a1c2410abc4564c3f86935b6e69c56291e9
DennyAzevedo <denny_af@yahoo.com.br>
2022-02-15 22:23:39 -0300

    Ajustando o visual - colocando icones - fontawesome

--- pro-atividade-app/src/App.js
+++ pro-atividade-app/src/App.js
@@ -57,12 +57,26 @@ function App() {
                     - Título
                   </h5>
                   <h6>
-                    Prioridade: Normal
+                    Prioridade:
+                    <span className="ms-1 text-black">
+                      <i className="me-1 fa-regular fa-face-frown"></i>
+                      Alta
+                    </span>
                   </h6>
                 </div>
                 <p className="card-text">
                   {ativ.descricao}
                 </p>
+                <div className="d-flex justify-content-end pt-2 m-0 border-top">
+                  <button className="btn btn-sm btn-outline-primary me-2">
+                    <i className="fas fa-pen me-2"></i>
+                    Editar
+                  </button>
+                  <button className="btn btn-sm btn-outline-danger">
+                  <i className="fas fa-trash me-2"></i>
+                    Deletar
+                  </button>
+                </div>
               </div>
             </div>
           ))}
