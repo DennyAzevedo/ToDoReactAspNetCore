e2ecf7dc0ace0475ab25d8933ea2d252c4674ed9
DennyAzevedo <denny_af@yahoo.com.br>
2022-05-06 10:32:38 -0300

    Criando Interfaces

---
+++ back/src/ProAtividade.Domain/Interfaces/Repositories/IGeralRepo.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProAtividade.Domain.Interfaces.Repositories {
+	public interface IGeralRepo {
+		void Acidionar<T>(T entity) where T: class;
+		void Atualizar<T>(T entity) where T: class;
+		void Deletar<T>(T entity) where T: class;
+		void DeletarVarias<T>(T[] entity) where T: class;
+		Task<bool> SalvarMudancasAsunc();
+	}
+}
